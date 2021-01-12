2bf2a7aedb26737d7f681ffeceb212a440910684
Albero Ricardo <aricardorodriguez@hotmail.com>
2021-01-12 23:12:26 +0100

    Added openCv image processor for face detection

--- ImageProcessor/include/ImageProcessor/image_processor_interface.h
+++ ImageProcessor/include/ImageProcessor/image_processor_interface.h
@@ -2,18 +2,45 @@
 #define _IMAGE_PROCESSOR_INTERFACE__H__
 #include <variant>
 #include <vector>
+#include <memory>
 
 namespace Multicamera {
 namespace ImageProcessor{
 
-using ImageProcessorResult = std::variant<std::vector<cv::Rect>>;
-
-template <typename Derived> struct ImageProcessBase {
-
-  template <typename Input> bool process(Input input_data) { return false; }
-
-  ImageProcessorResult image_processor_result_ = {};
+struct FrameGrey
+{
+cv::Mat frame_ ={};
 };
+
+struct FrameRGB
+{
+cv::Mat frame_ ={};
+};
+struct EyesRoi
+{
+std::vector<cv::Rect> roi_;
+};
+
+struct FaceRoi
+{
+ std::vector<cv::Rect> roi_;
+};
+
+
+
+template<typename >
+using ImageProcessorIO = std::variant<FaceRoi, EyesRoi, FrameRGB, FrameGrey>;
+
+template <typename Derived>
+struct ImageProcessBase {
+
+  ImageProcessorIO process(ImageProcessorIO input_data) noexcept{ 
+  return static_cast<Derived*>(this)->implementation(input_data);
+  }
+  
+  std::unique_ptr<cv::CascadeClassifier> classifiers_ = std::nullptr;
+};
+
 }
 } // namespace Multicamera
 #endif
