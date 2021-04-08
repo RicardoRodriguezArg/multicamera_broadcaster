98acd99a4a077b1d41605631f6c4c2081d157418
Alberto Ricardo Rodriguez <aricardorodriguez@hotmail.com>
2021-04-08 14:22:21 +0200

    add application directory

---
+++ Application/include/Application/application_utils.h
@@ -0,0 +1,81 @@
+#ifndef _VIDEO_CONTROLLER_H__
+#define _VIDEO_CONTROLLER_H__
+#include <atomic>
+#include <ctime>
+#include <memory>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <thread>
+
+namespace MultiCamera {
+namespace VideoController {
+namespace {
+static constexpr int RASPI_WEB_CAM = 0;
+static constexpr int MAX_RETRY_ATTEMP = 100;
+}  // namespace
+
+template <typename ImageProcessor>
+struct VideoControllerInfo {
+  explicit VideoControllerInfo(
+      const std::shared_ptr<ImageProcessor> &image_processor = nullptr)
+      : image_processor_(image_processor), capture_device_(RASPI_WEB_CAM) {}
+
+  ~VideoControllerInfo() {
+    stop();
+    video_thread_.join();
+  }
+
+  bool start() {
+    const boool is_valid_init = init();
+    if (!is_valid_init) {
+      return is_valid_init;
+    }
+    video_thread_ = std::thread(start_thread);
+  }
+
+  void stop() { is_working_ = false; }
+
+ private:
+  void start_thread() {
+    int retry_capture_attemp{0};
+    while (is_working_) {
+      capture_frame_.release();
+      if (!capture_device_.read(capture_frame_)) {
+        ++retry_capture_attemp;
+        if (retry_capture_attemp > MAX_RETRY_ATTEMP) {
+          is_working_ = false;
+        }
+      } else {
+        retry_capture_attemp = 0;
+#ifdef IMAGE_PROCESSOR_HAAR_DEBUG
+        image_processor_->detect(capture_frame_);
+        // show the frame in the created window
+        cv::imshow("Default Haar CV DEBUG ", capture_frame_);
+#endif
+#ifdef IMAGE_PROCESSOR_HAAR
+        image_processor_->detect(capture_frame_);
+#endif
+      }
+    }
+  }
+
+  bool init() {
+    if (!capture_device_.isOpened()) {
+      return false;
+    }
+    fps = capture_device_.get(cv::CAP_PROP_FPS);
+    return true;
+  }
+
+  double fps_{0.0};
+  std::thread video_thread_;
+  std::atomic<bool> is_working_{false};
+  cv::Frame capture_frame_{};
+  cv::VideoCapture capture_device_;  // capture the video from webcam
+  const std::shared_ptr<ImageProcessor> image_processor_;
+};
+}  // namespace VideoController
+}  // namespace MultiCamera
+#endif
---
+++ Application/src/application.cpp
@@ -0,0 +1,43 @@
+#include <VideoController/video_controller.h>
+#include <iostream>
+
+int main(int argc, char **argv) {
+  using namespace std;
+  using namespace cv;
+
+  VideoCapture cap(0); // capture the video from webcam
+
+  if (!cap.isOpened()) // if not success, exit program
+  {
+    cout << "Cannot open the web cam" << endl;
+    return -1;
+  } else {
+    std::cout << "Exito!" << std::endl;
+    double fps = cap.get(CAP_PROP_FPS);
+    std::cout << "Frames per seconds : " << fps << endl;
+    String window_name = "My First Video";
+    cv::Mat frame;
+    while (true) {
+
+      bool bSuccess = cap.read(frame); // read a new frame from video
+
+      // Breaking the while loop at the end of the video
+      if (bSuccess == false) {
+        cout << "Found the end of the video" << endl;
+        break;
+      }
+      // show the frame in the created window
+      imshow(window_name, frame);
+      frame.release();
+      // wait for for 10 ms until any key is pressed.
+      // If the 'Esc' key is pressed, break the while loop.
+      // If the any other key is pressed, continue the loop
+      // If any key is not pressed withing 10 ms, continue the loop
+      if (waitKey(10) == 27) {
+        cout << "Esc key is pressed by user. Stoppig the video" << endl;
+        break;
+      }
+    }
+  }
+  return 0;
+}
